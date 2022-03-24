3aab9f36cc6289f0beb136dfea2fde6db5c68e0d
Valerie <vcalle@colegiobolivar.org>
2022-03-24 16:42:44 -0400

    commit7

--- demo.js
+++ demo.js
@@ -1,3 +1,7 @@
 let yourName = prompt("Hi,como te llamas?");
-switch(yourName){case "Nicolas":
-console.log("Found the most amazing human being!");}
+switch(yourName)
+{case "Nicolas":
+console.log("Found the most amazing human being!");
+break
+case "Neni":
+    console.log("Found the love of my life.")
